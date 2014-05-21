27bc6e27b65e3beaacbaea3c49ca51fa29b85b0b
Todd Brannam <tbrannam@casualpenguin.com>
2014-05-21 13:08:24 -0400

    Update UIImage+MMLaunchImage.m
    added nil check to keyWindow -- and fall back to firstObject

--- UIImage+MMLaunchImage.m
+++ UIImage+MMLaunchImage.m
@@ -20,7 +20,14 @@
             orientationString = @"Landscape";
         }
         //filter down the array to just the matching elements
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"UILaunchImageMinimumOSVersion <= %@ AND UILaunchImageOrientation = %@ AND UILaunchImageSize = %@",os_version,orientationString,NSStringFromCGSize([UIApplication sharedApplication].keyWindow.bounds.size) ];
+        
+        UIWindow *mainWindow = [UIApplication sharedApplication].keyWindow;
+        if (mainWindow == nil) {
+          mainWindow = [[UIApplication sharedApplication].windows firstObject];
+        }
+
+        
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"UILaunchImageMinimumOSVersion <= %@ AND UILaunchImageOrientation = %@ AND UILaunchImageSize = %@",os_version,orientationString,NSStringFromCGSize(mainWindow.bounds.size) ];
         NSArray *suitableLaunchImages = [ios7LaunchImages filteredArrayUsingPredicate:predicate];
         NSString *imageName = [[suitableLaunchImages lastObject] objectForKey:@"UILaunchImageName"];
         UIImage *image = [UIImage imageNamed:imageName];
